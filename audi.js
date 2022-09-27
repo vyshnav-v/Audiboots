6d85c54e367e7b0724a7b7843a5bc3afc3c7c398
vaishnav <vaishnavvenugopal706@gmail.com>
2022-09-27 16:58:00 +0530

    final file

---
+++ audi.js
@@ -0,0 +1,8 @@
+const { buildFields } = require("express-fileupload/lib/utilities")
+
+$(document).ready(function(){
+    $("#head-link1").click(function(){
+        $("#head-actions").fadeToggle(3000)
+
+    })
+})
